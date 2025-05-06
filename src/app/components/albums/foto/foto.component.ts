1d73e06a79e2138997b25151feb4195731409fde
iScarcy <scarcella.giuseppe@gmail.com>
2025-05-06 22:36:53 +0200

    foto center into album

--- src/app/components/albums/foto/foto.component.ts
+++ src/app/components/albums/foto/foto.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppStateModel } from '../../../shared/store/Global/App.state';
 import { Store } from '@ngrx/store';
 import { selectToken } from '../../../shared/store/Login/login.selectors';
 import { loadalbumfoto } from '../../../shared/store/Albums/albums.actions';
+import { IFoto } from '../../../models/IFoto';
 
 @Component({
   selector: 'app-foto',
@@ -11,8 +12,19 @@ import { loadalbumfoto } from '../../../shared/store/Albums/albums.actions';
   templateUrl: './foto.component.html',
   styleUrl: './foto.component.css'
 })
-export class FotoComponent {
-  
 
+
+export class FotoComponent  implements OnInit{
+  ngOnInit(): void {
+   
+  }
+  
+  @Input() foto: IFoto = {
+    id: 0,
+    albumID: 0,
+    file: '',
+    thumbPathFile: '',
+    fullPathFile: ''
+  }
 
 }
