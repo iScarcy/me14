fb776ac661ad99b458064dfb04d6ac908d500457
iScarcy <56608800+iScarcy@users.noreply.github.com>
2025-07-11 17:28:49 +0200

    fix album gallery first view

--- src/app/components/albums/albums.component.ts
+++ src/app/components/albums/albums.component.ts
@@ -6,7 +6,7 @@ import { loadalbums } from '../../shared/store/Albums/albums.actions';
 import { Observable } from 'rxjs';
 import { IAlbumFoto } from '../../models/IAlbumFoto';
 import { getalbumslist } from '../../shared/store/Albums/albums.selectors';
-import { NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { refreshtoken } from '../../shared/store/Login/login.actions';
 
 @Component({
@@ -22,7 +22,7 @@ export class AlbumsComponent implements OnInit {
   brancaSelected : string | null  = "";
   albums$ = new Observable<IAlbumFoto[]> ();
   
-  constructor(private _store: Store<AppStateModel>, private _router: Router){
+  constructor(private _store: Store<AppStateModel>, private _router: ActivatedRoute ){
     
   }
  
@@ -32,78 +32,16 @@ export class AlbumsComponent implements OnInit {
     
      this.selectAlbums();
 
-     this._router.events.subscribe((event) => {
-
-      let branca:string = "";
-      
-      
-      if (event instanceof NavigationStart) {
-        branca = event.url.replace("/gallery/","");
-        if(branca!="" && 
+    this._router.params.subscribe((p) =>{
+      var branca = p["branca"];
+       if(branca!="" && 
             (branca=="lc" || branca=="eg" || branca=="rs" || branca=="varie")
           ){
             this.loadAlbums(branca);
           }
-
-        
-        console.log("nav start:"+ branca);
-      }
-      else  if (event instanceof NavigationEnd) {
-        console.log("nav end:");
-        
-        
-        
-      }
-      else  if (event instanceof NavigationError) {
-        console.log("nav err:"+ branca);
-      }
-     });
-
-   /*this._router.events.subscribe((event) => {
-      console.log("onInit albums")
-         if(this.brancaSelected!=null && 
-            (this.brancaSelected=="lc" || this.brancaSelected=="eg" || this.brancaSelected=="rs" || this.brancaSelected=="varie")
-          ){
-            this.loadAlbums(this.brancaSelected);
-          }
-          
-      if (event instanceof NavigationStart) {
-        // Navigation is starting... show a loading spinner perhaps?
-        // blog on that here: ultimatecourses.com/blog/angular-loading-spinners-with-router-events
-           console.log("start_")
-           debugger;
-           this.brancaSelected = event.url.replace("/gallery/","");
-          
-      
-         
-      }
-      if (event instanceof NavigationEnd) {
-        // We've finished navigating
-         debugger;
+    }); 
      
-         this.selectAlbums();
-      }
-      if (event instanceof NavigationError) {
-        // something went wrong, log the error
-          console.log(event.error);
-      }
-    });
-    */
-   
-    /*
-    switch(branca){
-      case "lc":  this.branca = "L/C";
-                  break;
-      case "eg":  this.branca = "E/G";
-                  break;
-      case "rs": this.branca = "R/S";
-                  break;            
-      case "varie": this.branca = "Gruppo";
-                  break;            
-    }
-    
-    this.loadAlbums(branca);
-    */
+
   }
  
  
--- src/app/components/utility/utility.component.ts
+++ src/app/components/utility/utility.component.ts
@@ -29,33 +29,7 @@ export class UtilityComponent implements OnInit{
       var type = p["type"];
       this._service.getUtility(type).subscribe((data) => {this.utility = data.sort((a, b) => a.typeID - b.typeID);});
     }); 
-    /*
-     this._router.events.subscribe((event) => {
-            
-            if (event instanceof NavigationStart) {
-            
-        
-              console.log("utility start:"+ event.url);
-
-              var type:string =  event.url.replace("/utility/","");
-              this._service.getUtility(type).subscribe((data) => {this.utility = data.sort((a, b) => a.typeID - b.typeID);});
-            }
-            else  if (event instanceof NavigationEnd) {
-              console.log("utility end:");
-              
-              
-              
-            }
-            else  if (event instanceof NavigationError) {
-              console.log("utility err:" );
-            }
-            else{
-              console.log(event.type);
-              console.log(event.toString());
-            }  
-
-     });
-    */
+   
   }
 
 
