c59785c31311e15335cbbfe667d9067bac35f0a6
iScarcy <56608800+iScarcy@users.noreply.github.com>
2025-07-28 14:17:46 +0200

    news page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { AlbumsComponent } from './components/albums/albums.component';
 import { AlbumComponent } from './components/albums/album/album.component';
 import { UtilityComponent } from './components/utility/utility.component';
 import { VideoComponent } from './components/video/video.component';
+import { ReelComponent } from './components/reel/reel.component';
  
 const routes: Routes = [
   {path:"", component: MainContentComponent},
   {path:"video", component: VideoComponent},
+  {path:"news", component: ReelComponent},
   {path:"utility/:type", component: UtilityComponent},
   {path:"gallery/:branca", component: AlbumsComponent},
   {path:"gallery/:branca/:album", component: AlbumComponent},
