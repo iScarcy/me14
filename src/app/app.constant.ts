2df8dae1b48e0a1c111d91fba87f3f94ca2893b0
iScarcy <scarcella.giuseppe@gmail.com>
2025-09-25 19:09:54 +0200

    https url service

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -1,9 +1,9 @@
-export const baseGalleryApiUrl:string = "http://www.agescimessina14.org/fw/api/Gallery/";
-export const baseGalleryPublicImageUrl: string = "http://www.agescimessina14.org/public/gallery/"
-export const baseHomePagePublicImageUrl: string = "http://www.agescimessina14.org/public/fotoHP/"
-export const baseUtilityPublicImageUrl: string = "http://www.agescimessina14.org/public/utility/"
-export const baseUsersApiUrl:string = "http://www.agescimessina14.org/fw/api/Users/";
-export const baseUtilityApiUrl:string = "http://www.agescimessina14.org/fw/api/Utility/";
+export const baseGalleryApiUrl:string = "https://www.agescimessina14.org/fw/api/Gallery/";
+export const baseGalleryPublicImageUrl: string = "https://www.agescimessina14.org/public/gallery/"
+export const baseHomePagePublicImageUrl: string = "https://www.agescimessina14.org/public/fotoHP/"
+export const baseUtilityPublicImageUrl: string = "https://www.agescimessina14.org/public/utility/"
+export const baseUsersApiUrl:string = "https://www.agescimessina14.org/fw/api/Users/";
+export const baseUtilityApiUrl:string = "https://www.agescimessina14.org/fw/api/Utility/";
 
 export const domain_user:string      = "messina14_webSite";
 export const secret_id:string   = "dZp4J6qgqcxQMUtB4QPPVQ==";
